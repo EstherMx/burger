be9a11de1206297337aa5996ec241e1b241af8e4
Esther Maxi <esmaxi21@gmail.com>
2017-05-23 17:15:58 -0400

    uploaded

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('chicken bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('double cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('quarter pounder burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('veggie burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('salmon burger', true);
